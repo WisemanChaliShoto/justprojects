89129c0957b5dccbb5251546f20e11fca8a3686d
Chali <charlie.shoto"gmail.com>
2023-06-11 17:04:29 +0200

    With Comments

--- .vscode/Click me/script.js
+++ .vscode/Click me/script.js
@@ -2,9 +2,9 @@ const button = document.querySelector('button')
 const body = document.querySelector('body')
 const color = ['red','blue','pink','yellow','white']
 
- body.style.backgroundColor = 'yellow'
+ body.style.backgroundColor = 'yellow' // background color before clicking
 
-
+// Function which changes colors randomly 
 
 const changeColor = () => {
     const colorIndex = parseInt(Math.random()*color.length)
